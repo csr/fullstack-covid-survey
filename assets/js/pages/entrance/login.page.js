d977b9fb7de76b6846ac0b39dbe3ce3c7e0535e3
Cesare De Cal <hi@cesare.io>
2020-03-30 16:34:52 +0200

    Bug fixes in login

--- assets/js/pages/entrance/login.page.js
+++ assets/js/pages/entrance/login.page.js
@@ -3,27 +3,20 @@ parasails.registerPage('login', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
+
     // Main syncing/loading state for this page.
     syncing: false,
 
     // Form data
-    formData: {
-      rememberMe: true,
-    },
+    formData: { /* … */ },
 
     // For tracking client-side validation errors in our form.
     // > Has property set to `true` for each invalid property in `formData`.
     formErrors: { /* … */ },
 
-    // A set of validation rules for our form.
-    // > The form will not be submitted if these are invalid.
-    formRules: {
-      emailAddress: { required: true, isEmail: true },
-      password: { required: true },
-    },
-
     // Server error state for the form
     cloudError: '',
+
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -33,8 +26,8 @@ parasails.registerPage('login', {
     // Attach any initial data from the server.
     _.extend(this, SAILS_LOCALS);
   },
-  mounted: async function() {
-    //…
+  mounted: function() {
+    this.$focus('[autofocus]');
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -42,12 +35,48 @@ parasails.registerPage('login', {
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
 
-    submittedForm: async function() {
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Note:
+    // To see what this would look like as a completely custom form, without relying on as many
+    // built-in features of the <ajax-form> component, see:
+    // https://github.com/sailshq/caviar/commit/512eb41c347f9bcdebc2d1bca8b15d8a0acd80f1
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    submittedForm: function() {
+
       // Redirect to the logged-in dashboard on success.
       // > (Note that we re-enable the syncing state here.  This is on purpose--
       // > to make sure the spinner stays there until the page navigation finishes.)
       this.syncing = true;
       window.location = '/';
+
+    },
+
+    handleParsingForm: function() {
+
+      // Clear out any pre-existing error messages.
+      this.formErrors = {};
+
+      var argins = this.formData;
+
+      // Validate email:
+      if(!argins.emailAddress) {
+        this.formErrors.emailAddress = true;
+      }
+
+      // Validate password:
+      if(!argins.password) {
+        this.formErrors.password = true;
+      }
+
+      // If there were any issues, they've already now been communicated to the user,
+      // so simply return undefined.  (This signifies that the submission should be
+      // cancelled.)
+      if (Object.keys(this.formErrors).length > 0) {
+        return;
+      }
+
+      return argins;
     },
 
   }
