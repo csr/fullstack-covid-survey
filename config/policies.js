4f3263ab8f0324896351574eb4e7af683b7c6188
Cesare De Cal <hi@cesare.io>
2020-04-05 11:21:01 +0200

    deleted policy

--- config/policies.js
+++ config/policies.js
@@ -8,20 +8,20 @@
  * https://sailsjs.com/docs/concepts/policies
  */
 
-module.exports.policies = {
+// module.exports.policies = {
 
-  '*': 'is-logged-in',
+//   '*': 'is-logged-in',
 
-  // Bypass the `is-logged-in` policy for:
-  'view-map': true,
-  'entrance/*': true,
-  'account/logout': true,
-  'view-homepage-or-redirect': true,
-  'view-faq': true,
-  'view-contact': true,
-  'legal/view-terms': true,
-  'legal/view-privacy': true,
-  'deliver-contact-form-message': true,
-  'security/grant-csrf-token': true,
-  'create-report': true,
-};
+//   // Bypass the `is-logged-in` policy for:
+//   'view-map': true,
+//   'entrance/*': true,
+//   'account/logout': true,
+//   'view-homepage-or-redirect': true,
+//   'view-faq': true,
+//   'view-contact': true,
+//   'legal/view-terms': true,
+//   'legal/view-privacy': true,
+//   'deliver-contact-form-message': true,
+//   'security/grant-csrf-token': true,
+//   'create-report': true,
+// };
