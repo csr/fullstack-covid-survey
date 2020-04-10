b9e69f1aef645d7484a3f8a67136105971d9f8c2
Cesare De Cal <hi@cesare.io>
2020-04-10 15:00:15 +0200

    Minor clean up

--- assets/js/pages/map.page.js
+++ assets/js/pages/map.page.js
@@ -1,106 +1,133 @@
-parasails.registerPage('map', {
-  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
-  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
-  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
-  data: {
-    reports: [],
+  parasails.registerPage('map', {
+    //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+    //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+    //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+    data: {
+      reports: [],
 
-    promptTrackSymptomsModalOpen: false,
+      surveyContainerOpen: false,
 
-    // Syncing/loading state
-    syncing: false,
+      states: {
+        'anySymptoms': {
+          title: 'Hai sintomi del COVID-19?',
+          subtitle: 'Per esempio febbre, tosse, affanno, dolori muscolari.',
+          options: [
+            "yes",
+            "no"
+          ],
+        },
+        'feverTemperature': {
+          title: 'Hai la febbre?',
+          subtitle: 'La febbre è uno dei principali sintomi del COVID-19.',
+          options: [
+            "yes",
+            "no"
+          ],
+        },
+      },
 
-    // Server error state
-    cloudError: '',
-  },
+      formAnswers: {
 
-  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
-  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
-  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
-  beforeMount: function() {
-    // Attach any initial data from the server.
-    _.extend(this, SAILS_LOCALS);
-    this.reports = this._marshalEntries(this.reports);
-  },
+      },
 
-  mounted: async function() {
-    console.log("I'm inside mounted!");
+      promptTrackSymptomsModalOpen: false,
 
-    var map = L.map('mapid').setView([51.505, -0.09], 3);
+      // Syncing/loading state
+      syncing: false,
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        accessToken: 'pk.eyJ1IjoiY2VzYXJlZGVjYWwiLCJhIjoiY2s0aDgydnk3MTFkazNsbjZsc3B0MXo5ZSJ9.d7fi1XCpfi5q9YUEN1xuBg'
-    }).addTo(map);
+      // Server error state
+      cloudError: '',
+    },
 
-    var layerGroup = L.markerClusterGroup().addTo(map);
+    //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+    //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+    //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+    beforeMount: function() {
+      // Attach any initial data from the server.
+      _.extend(this, SAILS_LOCALS);
+      this.reports = this._marshalEntries(this.reports);
+    },
 
-    var heatLayer = L.heatLayer([
-    ], {radius: 100, gradient: {0.4: 'blue', 0.65: 'lime', 1: 'red'}}).addTo(layerGroup);
+    mounted: async function() {
+      console.log("I'm inside mounted!");
 
-    function addPinToMap(value, index, array) {
-      L.marker([value.lat, value.long]).addTo(map)
-          .bindPopup(value.completionDate.toString());
-      var latlng = L.latLng(value.lat, value.long, 1);
-      heatLayer.addLatLng(latlng);
-    }
+      var map = L.map('mapid').setView([51.505, -0.09], 3);
 
-    this.reports.forEach(addPinToMap);
-    console.log('reports:', this.reports);
+      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          accessToken: 'pk.eyJ1IjoiY2VzYXJlZGVjYWwiLCJhIjoiY2s0aDgydnk3MTFkazNsbjZsc3B0MXo5ZSJ9.d7fi1XCpfi5q9YUEN1xuBg'
+      }).addTo(map);
 
-    var x = document.getElementById("surveycontainer");
-    x.style.display = "none";
-  },
+      var layerGroup = L.markerClusterGroup().addTo(map);
 
-  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
-  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
-  methods: {
-    myFunction: function(event) {
-      console.log('Hello world!', event);
-      var x = document.getElementById("surveycontainer");
-      
-      if (x.style.display === "none") {
-        console.log('Ok, showing survey.')
-        // Show survey
-        x.style.display = "block";
-        // Show cross
-        $('#crossicon').removeClass('buttonHideIconAnimation');
-        // Remove chat icon
-        $('#chaticon').addClass('buttonHideIconAnimation');
-      } else {
-        console.log('Ok, hiding survey.')
-        // Hide survey
-        x.style.display = "none";
-        // Hide cross
-        $('#crossicon').addClass('buttonHideIconAnimation');
-        // Show chat icon
-        $('#chaticon').removeClass('buttonHideIconAnimation');
+      var heatLayer = L.heatLayer([
+      ], {radius: 100, gradient: {0.4: 'blue', 0.65: 'lime', 1: 'red'}}).addTo(layerGroup);
+
+      function addPinToMap(value, index, array) {
+        L.marker([value.lat, value.long]).addTo(map)
+            .bindPopup(value.completionDate.toString());
+        var latlng = L.latLng(value.lat, value.long, 1);
+        heatLayer.addLatLng(latlng);
       }
+
+      this.reports.forEach(addPinToMap);
+      console.log('reports:', this.reports);
     },
 
-    _marshalEntries: function(entries) {
-      // Marshal provided array of data and return the modified version.
-      return _.map(entries, (entry)=>{
-        return entry;
-      });
-    },
+    //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+    //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+    methods: {
+      clickOpenSurveyButton: function(event) {
+        var x = document.getElementById("surveycontainer");
+        
+        if (!this.surveyContainerOpen) {
+          console.log('Ok, showing survey.')
+          this.surveyContainerOpen = true;
+          // Show cross
+          $('#crossicon').removeClass('buttonHideIconAnimation');
+          // Remove chat icon
+          $('#chaticon').addClass('buttonHideIconAnimation');
 
-    closeTrackSymptomsModal: function() {
-      this.promptTrackSymptomsModalOpen = false;
-    },
+        } else {
+          console.log('Ok, hiding survey.')
+          this.surveyContainerOpen = false;
+          // Hide cross
+          $('#crossicon').addClass('buttonHideIconAnimation');
+          // Show chat icon
+          $('#chaticon').removeClass('buttonHideIconAnimation');
+        }
+      },
 
-    handleParsingTrackSymptomsModal: function() {
-      return {
-        id: this.selectedThing.id
-      };
-    },
+      _marshalEntries: function(entries) {
+        // Marshal provided array of data and return the modified version.
+        return _.map(entries, (entry)=>{
+          return entry;
+        });
+      },
 
-    submittedTrackSymptomsModal: function() {
-      console.log('ok it worked!');
-      this.$forceUpdate();
-    },
-  }
-});
+      closeTrackSymptomsModal: function() {
+        this.promptTrackSymptomsModalOpen = false;
+      },
+
+      handleParsingTrackSymptomsModal: function() {
+        return {
+          id: this.selectedThing.id
+        };
+      },
+
+      submittedTrackSymptomsModal: function() {
+        console.log('ok it worked!');
+        this.$forceUpdate();
+      },
+
+      radioclicked: function(event) {
+        console.log('well, radio clicked', event);
+        var context = event.toElement.name;
+        var answer = event.toElement.value;
+        console.log('context: ', context, ', answer: ', answer);
+      },
+    }
+  });
